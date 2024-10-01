59a84efe626fc5b1d39606fac5640f658af3fc2f
NT912 <tt912002@gmail.com>
2024-10-01 14:21:20 +0700

    Finished lab3/view_template

--- mysite/tutorial/apps.py
+++ mysite/tutorial/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MyapplicationConfig(AppConfig):
+class TutorialConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'myapp'
+    name = 'tutorial'
